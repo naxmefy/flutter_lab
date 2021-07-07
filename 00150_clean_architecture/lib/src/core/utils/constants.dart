bba7ce105bfb1432af7c47530989cfb4a618919a
MRW Neundorf <ninevillage@gmail.com>
2021-07-08 00:44:16 +0200

    feat(00150): added constants, routes and theme

---
+++ 00150_clean_architecture/lib/src/core/utils/constants.dart
@@ -0,0 +1,11 @@
+const String kMaterialAppTitle = 'Flutter Clean Architecture';
+
+// API
+const String kBaseUrl = 'https://newsapi.org/v2';
+
+const String kApiKey = '2089c8a7efd24735a84ceaa8f1f77893';
+
+// Database
+const String kArticlesTableName = 'articles_table';
+
+const String kDatabaseName = 'app_database.db';
