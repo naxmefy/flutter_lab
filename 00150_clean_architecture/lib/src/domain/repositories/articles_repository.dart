28e33e65c23868c67f6d61730c3e3f0a8d241f4a
Mohammed Neundorf <mohammed.neundorf@sgd.de>
2021-07-08 09:35:47 +0200

    feat(00150): resolve get_articles_usecase

--- 00150_clean_architecture/lib/src/domain/repositories/articles_repository.dart
+++ 00150_clean_architecture/lib/src/domain/repositories/articles_repository.dart
@@ -1,6 +1,9 @@
+import 'package:cleanarchitecture/src/core/params/articles_request.dart';
 import 'package:cleanarchitecture/src/core/resources/data_state.dart';
+import 'package:cleanarchitecture/src/domain/entities/article.dart';
 
 abstract class ArticlesRepository {
   Future<DataState<List<Article>>> getBreakingNewsArticles(
-      ArticlesRequestParams params);
+    ArticlesRequestParams params,
+  );
 }
--- 00150_clean_architecture/lib/src/domain/usecases/get_articles_usecase.dart
+++ 00150_clean_architecture/lib/src/domain/usecases/get_articles_usecase.dart
@@ -1,6 +1,19 @@
+import 'package:cleanarchitecture/src/core/params/articles_request.dart';
+import 'package:cleanarchitecture/src/core/resources/data_state.dart';
+import 'package:cleanarchitecture/src/core/usecases/usecase.dart';
+import 'package:cleanarchitecture/src/domain/entities/article.dart';
+import 'package:cleanarchitecture/src/domain/repositories/articles_repository.dart';
+
 class GetArticlesUseCase
     implements UseCase<DataState<List<Article>>, ArticlesRequestParams> {
   final ArticlesRepository _articlesRepository;
 
-  GetA
+  GetArticlesUseCase(this._articlesRepository);
+
+  @override
+  Future<DataState<List<Article>>> call({
+    required ArticlesRequestParams params,
+  }) {
+    return _articlesRepository.getBreakingNewsArticles(params);
+  }
 }
